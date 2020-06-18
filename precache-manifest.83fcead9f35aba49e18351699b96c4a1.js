f83660217322e6669e5bb4c7cbb60b1297216f3a
blasius18 <blasius18@gmail.com>
2020-06-18 12:17:47 +0530

    Updates

--- precache-manifest.83fcead9f35aba49e18351699b96c4a1.js
+++ precache-manifest.83fcead9f35aba49e18351699b96c4a1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77e339d865ef09c6c0b1392cb075864a",
+    "revision": "cf83842a88cf24599e5d97b6b0f9e466",
     "url": "/ilrnu-master/index.html"
   },
   {
-    "revision": "bc1fcd4792efa50abdd3",
+    "revision": "111775ec72b66b8b493d",
     "url": "/ilrnu-master/static/css/2.55146207.chunk.css"
   },
   {
-    "revision": "74098caf2461118378e3",
+    "revision": "dd9b7619ccff03bd39de",
     "url": "/ilrnu-master/static/css/main.5753724c.chunk.css"
   },
   {
-    "revision": "bc1fcd4792efa50abdd3",
-    "url": "/ilrnu-master/static/js/2.467f2e56.chunk.js"
+    "revision": "111775ec72b66b8b493d",
+    "url": "/ilrnu-master/static/js/2.dc49a9ff.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/ilrnu-master/static/js/2.467f2e56.chunk.js.LICENSE.txt"
+    "url": "/ilrnu-master/static/js/2.dc49a9ff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "74098caf2461118378e3",
-    "url": "/ilrnu-master/static/js/main.569a0ed8.chunk.js"
+    "revision": "dd9b7619ccff03bd39de",
+    "url": "/ilrnu-master/static/js/main.62b2bcc0.chunk.js"
   },
   {
     "revision": "8fad54fb20405c8db7aa",
