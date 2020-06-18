c66a7112966b7dbec0599465474c34af3731c7bb
blasius18 <blasius18@gmail.com>
2020-06-18 11:31:23 +0530

    Updates

---
+++ precache-manifest.d6d00a519b64c6d6a13a374ddbf86c4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1539639f2eddfee380413b8de85bcbed",
+    "url": "/ilrnu-master/index.html"
+  },
+  {
+    "revision": "bc1fcd4792efa50abdd3",
+    "url": "/ilrnu-master/static/css/2.55146207.chunk.css"
+  },
+  {
+    "revision": "631f4004a44cd8e9f271",
+    "url": "/ilrnu-master/static/css/main.5753724c.chunk.css"
+  },
+  {
+    "revision": "bc1fcd4792efa50abdd3",
+    "url": "/ilrnu-master/static/js/2.467f2e56.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ilrnu-master/static/js/2.467f2e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631f4004a44cd8e9f271",
+    "url": "/ilrnu-master/static/js/main.247c0987.chunk.js"
+  },
+  {
+    "revision": "8fad54fb20405c8db7aa",
+    "url": "/ilrnu-master/static/js/runtime-main.cd1dca88.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5fcbcc697359cb420877ddaf572d226.js"
+  "/ilrnu-master/precache-manifest.d6d00a519b64c6d6a13a374ddbf86c4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ilrnu-master/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
